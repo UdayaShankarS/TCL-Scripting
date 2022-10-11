e2091c702bf0325df8aa676a80bc406c95b33ca3
UdayaShankarS <93757421+UdayaShankarS@users.noreply.github.com>
2022-10-11 17:59:53 +0530

    Update TCL script to find length of string without using command string length.tcl

--- TCL script to find length of string without using command string length.tcl
+++ TCL script to find length of string without using command string length.tcl
@@ -5,6 +5,6 @@
 
 
 # Write a TCL script to find the length of the string without using built-in command "string length"
-set str "TCL SCRIPTING"
-puts [set k [string range $str end end]]
-puts "The length of the string is: [expr [string last "$k" $str] + 1]"
+set str "TCL SCRIPTING" ; # input string
+puts [set k [string range $str end end]]  ; # set variable k with last element/character of the string
+puts "The length of the string is: [expr [string last "$k" $str] + 1]"  ; # To fine the length of the string the position of the last charcter is incremented with 1
