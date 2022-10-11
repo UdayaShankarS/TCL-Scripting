cefd0027de18125072ac2df612807ce80751b93b
UdayaShankarS <93757421+UdayaShankarS@users.noreply.github.com>
2022-10-11 17:58:07 +0530

    Update TCL script to find length of list without using command llength.tcl

--- TCL script to find length of list without using command llength.tcl
+++ TCL script to find length of list without using command llength.tcl
@@ -4,6 +4,6 @@
 
 
 #Write a TCL script to find the length of the string without using built-in command "llength"
-set lin { 5 6 4 8 9 a}
-puts [set z [lrange $lin end end]]
-puts "The length of the list is: [expr [lsearch $lin "$z" ] + 1]"
+set lin { 5 6 4 8 9 a}  ; # input list 'lin'
+puts [set z [lrange $lin end end]]  ; # assign z with elements at end / last element
+puts "The length of the list is: [expr [lsearch $lin "$z" ] + 1]" ; # To display the length of the list the position of last element is incremented to one.
