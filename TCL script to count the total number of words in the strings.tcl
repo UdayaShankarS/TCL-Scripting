5fd9ea9b5855785e54ffea9769b2f47d43cbe477
Dr. Udaya Shankar S M.E., Ph.D <93757421+UdayaShankarS@users.noreply.github.com>
2022-11-10 16:02:50 +0530

    Create TCL script to count the total number of words in the strings.tcl

---
+++ TCL script to count the total number of words in the strings.tcl
@@ -0,0 +1,17 @@
+#Written by : Udaya Shankar S
+#dated: 8.11.2022
+# TCL script to count the total number of words in the strings.
+#Version 1.0
+
+
+set in "Tcl Runs on Windows, Mac OS X, Linux, and virtually every variant of unix. Event driven programming. Tcl is a cross platform language with full unicode support."
+puts [set l [string length $in]]
+set cnt 0
+for {set i 0} {$i < $l} {incr i} {
+    if { [string equal -nocase [string index $in $i] " "]  == 1 } {
+        incr cnt
+    } else {
+        continue
+    }
+}
+puts "No of words: [expr $cnt+1]"
