abf9b52b24a66cf111c25f5c32ca5900aceebc10
Dr. Udaya Shankar S M.E., Ph.D <93757421+UdayaShankarS@users.noreply.github.com>
2022-11-11 16:36:07 +0530

    Create TCL script to find the difference between the two lists.tcl

---
+++ TCL script to find the difference between the two lists.tcl
@@ -0,0 +1,46 @@
+#Written by : Udaya Shankar S
+#dated: 9.11.2022
+#TCL script to find the difference between the two lists
+#Version 1.0
+
+set a { 10 12 14 16 18 20 21}
+set b { 10 12 15 16 18 20 25 27 28}
+if {[llength $a] == [llength $b]} {
+  for {set i 0 } {$i < [llength $a] } {incr i} {
+      for {set j 0} {$j < [llength $b] } {incr j} {
+            if {[lindex $a $i] == [lindex $b $i]} {
+                puts "Lists a & b are equal at $i"
+                break
+            } else {
+                puts "Lists a & b are notequal at $i" 
+                break
+            }
+      }
+  }
+} elseif {[llength $a] > [llength $b]} {
+  for {set i 0 } {$i < [llength $a] } {incr i} {
+      for {set j 0} {$j < [llength $a] } {incr j} {
+            if {[lindex $a $i] == [lindex $b $i]} {
+                puts "Lists a & b are equal at $i"
+                break
+            } else {
+                puts "Lists a & b are notequal at $i" 
+                break
+            }
+      }
+  }
+} elseif {[llength $a] < [llength $b]} {
+  for {set i 0 } {$i < [llength $b] } {incr i} {
+      for {set j 0} {$j < [llength $b] } {incr j} {
+            if {[lindex $a $i] == [lindex $b $i]} {
+                puts "Lists a & b are equal at $i"
+                break
+            } else {
+                puts "Lists a & b are notequal at $i"   
+                break
+            }
+      }
+  }
+} else {
+    puts "List a and b not of equal length"
+}
