4400cba3d76f9978c97b6c8dda1b26dd59821278
Dr. Udaya Shankar S M.E., Ph.D <93757421+UdayaShankarS@users.noreply.github.com>
2022-12-15 11:06:36 +0530

    Update List of IO Pins from Netlist.tcl

--- TCL Scripts for Physical Design/List of IO Pins from Netlist.tcl
+++ TCL Scripts for Physical Design/List of IO Pins from Netlist.tcl
@@ -4,20 +4,22 @@
 #Version:1.0
 
 
-
-set inf [open mycode.v r]
-set io {}
-while {[gets $inf line] >= 0} {
-	if {[regexp -all "input*" $line m]} {
-		set x [regexp -all {[^input]+\w+[^:;]+} $line match2 ];
-		#puts $match2
-		set io [append io "$match2" ","]
-	} elseif {[regexp -all "output*" $line n]} {
-		set y [regexp -all {[^output]+\w+[^:;]+} $line match3 ];
-		#puts $match3;
-		set io [append io "$match3" ","]
-	} else {}
+proc NoofIOs {in} {
+	set inf [open $in r]
+	set io {}
+	while {[gets $inf line] >= 0} {
+		if {[regexp -all "input*" $line m]} {
+			set x [regexp -all {[^input]+\w+[^:;]+} $line match2 ];
+			#puts $match2
+			set io [append io "$match2" ","]
+		} elseif {[regexp -all "output*" $line n]} {
+			set y [regexp -all {[^output]+\w+[^:;]+} $line match3 ];
+			#puts $match3;
+			set io [append io "$match3" " "]
+		} else {}
+	}
+	puts "List of Nets: $io"
+	puts "Total No. of I/Os: [llength $io]"
+	close $inf
 }
-puts "List of Nets: $io"
-puts "Total No. of I/Os: [llength $io]"
-close $inf
+NoofIOs "mycode.v"
