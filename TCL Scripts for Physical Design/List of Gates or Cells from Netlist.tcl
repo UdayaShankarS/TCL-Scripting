af9deb048e128679f326cdd4884389af9c75893f
Dr. Udaya Shankar S M.E., Ph.D <93757421+UdayaShankarS@users.noreply.github.com>
2022-12-15 11:05:59 +0530

    Update List of Gates or Cells from Netlist.tcl

--- TCL Scripts for Physical Design/List of Gates or Cells from Netlist.tcl
+++ TCL Scripts for Physical Design/List of Gates or Cells from Netlist.tcl
@@ -3,20 +3,23 @@
 #TCL Script to Fine the total number and list of Gates/standard cells in Verilog Netlist
 #Version:1.0
 
-
-set inf [open mycode.v r]
-set g {}
-set linenumber 0
-while {[gets $inf line] >= 0} {
-	if {[regexp -all "and+" $line m] | [regexp -all "or+" $line m] | [regexp -all "nor+" $line m] |[regexp -all "not+" $line m] } {
-		set k [lindex $line 1]
-		puts $k
-		set g [append g "$k" " "]
-	} elseif {[regexp -all "endmodule*" $line n]} {
-		break
-	} else {}
-#incr linenumber
+proc NoofGates {in} {
+	set inf [open $in r]
+	set g {}
+	set linenumber 0
+	while {[gets $inf line] >= 0} {
+		if {[regexp -all "and+" $line m] | [regexp -all "or+" $line m] | [regexp -all "nor+" $line m] |[regexp -all "not+" $line m] } {
+			set k [lindex $line 1]
+			puts $k
+			set g [append g "$k" " "]
+		} elseif {[regexp -all "endmodule*" $line n]} {
+			break
+		} else {}
+	#incr linenumber
+	}
+	puts "List of Gates: $g"
+	puts "Total No. of Gates: [llength $g]"
+	close $inf
 }
-puts "List of Gates: $g"
-puts "Total No. of Gates: [llength $g]"
-close $inf
+
+NoofGates "mycode.v"
