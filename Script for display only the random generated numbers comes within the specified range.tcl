4f9e5bf24397659677c396d243ec92a2c3d26396
Dr. Udaya Shankar S M.E., Ph.D <93757421+UdayaShankarS@users.noreply.github.com>
2022-12-22 12:09:00 +0530

    Update Script for display only the random generated numbers comes within the specified range.tcl

--- Script for display only the random generated numbers comes within the specified range.tcl
+++ Script for display only the random generated numbers comes within the specified range.tcl
@@ -3,6 +3,7 @@
 # Tcl procedure to display only the random generated numbers comes within the specified range
 #Version 1.0
 
+# Method 1
 proc random {} {
 set a [expr rand()*10] 
 if { 0 <= $a && $a <= 8} {
@@ -11,3 +12,18 @@ if { 0 <= $a && $a <= 8} {
 
 }
 random
+
+# Method 2
+
+proc min_max {min max} {
+      #set a [expr { rand() }]
+      set a [ expr {(rand())*25 }]
+      puts "Min: $min Max: $max"
+      set a [expr int ($a)]
+  if { [expr $min <= $a] && [expr $a <= $max ] } {
+          puts "[expr int ($a)] is within $min & $max"
+  } else {
+          puts "$a is not within $min & $max"
+  }
+}
+min_max 10 100
