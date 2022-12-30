145a9b4f3b5bfe824b392c2b89061381bd436d4d
Dr. Udaya Shankar S M.E., Ph.D <93757421+UdayaShankarS@users.noreply.github.com>
2022-12-30 13:00:20 +0530

    Update Tcl_Proc_findDutyCycle_Freq_PeriodfromSDC.tcl

--- TCL Scripts for Physical Design/Tcl_Proc_findDutyCycle_Freq_PeriodfromSDC.tcl
+++ TCL Scripts for Physical Design/Tcl_Proc_findDutyCycle_Freq_PeriodfromSDC.tcl
@@ -3,7 +3,6 @@
 # TCL procedure to find the duty cycle, frequency and time period from the SDC constrants file
 # Version:1.0
 
-
 proc DCFTP {in} {
 	set inf [open "$in" r]
 	while {[gets $inf line] >= 0} {
@@ -11,13 +10,22 @@ proc DCFTP {in} {
 			set x [regexp -all {[^period*]+\w+\w+[*.0]+\w} $line match2 ];
 			puts "Time Period: $match2 ns"
 			puts "Frequency : [expr 1/$match2] GHz"
-			set y [regexp -all {\w[^0.]+[*.0]\w} $line match3 ];
-			#puts "$match3" ; 
-			set D [expr [lindex $match3 1]/$match2 ]
+			#set y [regexp -all {\d[^0.]+[*.0]\d} $line match3 ];
+			set y [regexp -all {\d[^waveform]+[*.0]\d} $line match3 ];
+			puts "$match3" ; 
+			set e [expr [lindex $match3 0]+[lindex $match3 1]]
+			set D [expr $e/$match2 ]
 			puts "Duty Cycle: [expr $D*100]%"
+			break
 		} else {}
 	}
-	#close $inf
+	close $inf
 }
 
 DCFTP "sample_SDC.sdc"
+
+# sample Output
+# Time Period:  16.0 ns
+# Frequency : 0.0625 GHz
+# 0.0 8.0
+# Duty Cycle: 50.0%
